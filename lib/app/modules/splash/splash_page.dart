01fa0fec26ad73b97d978affa48ecb0d56ffb2f6
Antönio Andson <aads.andsonsilva@gmail.com>
2022-03-07 03:05:48 -0300

    Front: Splash + HomePage

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:vakinha_burger_mobile/app/core/common/constants/texts_constant.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_appbar.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_button.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_textformfield.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -6,7 +10,18 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Splash'),),
+      appBar: VakinhaAppbar(),
+      body: Column(
+        children: const [
+          SizedBox(height: 20,),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: VakinhaTextformfield(label: 'text Form',),
+          ),
+          SizedBox(height: 20,),
+          VakinhaButton(onPressed: null, label: TextsConstant.entrar)
+        ],
+      ),
     );
   }
 }
